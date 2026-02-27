f557924d7f49d352dab0f4aa18dc0a4d0ed216e7
Divya Dharshini <addharshini1998@gmail.com>
2026-02-26 22:51:37 -0800

    Add security POC workflow (monitor mode)

--- src/test_secret.py
+++ src/test_secret.py
@@ -1,3 +1,3 @@
 aws_access_key_id = "AKIA1234567890ABCDE"
 aws_secret_access_key = "abcdEFGHijklMNOPqrstUVWXyz1234567890abcd"
-password = "SuperSecretPassword123!"
+password = "SuperSecretPassworkjkjd123!"
